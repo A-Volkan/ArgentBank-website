8ca6a11603422ce1f08a93ec8a337d1166c1c3db
Asci Volkan <volknasc735@gmail.com>
2023-10-25 19:34:44 +0200

    modif

--- my-app/src/Pages/Userprofil.js
+++ my-app/src/Pages/Userprofil.js
@@ -3,6 +3,7 @@ import './Userprofil.css';
 import Navbar from '../Components/Navbar';
 import Footer from '../Components/Footer';
 import WelcomeBack from '../Components/welcomeBack';
+import Account from '../Components/Account';
 
 
 
@@ -13,37 +14,27 @@ const Userprofil = () => {
             <Navbar />
             <main className="main bg-dark">
                 <WelcomeBack />
-                <h2 className="sr-only">Accounts</h2>
-                <section className="account">
-                    <div className="account-content-wrapper">
-                        <h3 className="account-title">Argent Bank Checking (x8349)</h3>
-                        <p className="account-amount">$2,082.79</p>
-                        <p className="account-amount-description">Available Balance</p>
-                    </div>
-                    <div className="account-content-wrapper cta">
-                        <button className="transaction-button">View transactions</button>
-                    </div>
-                </section>
-                <section className="account">
-                    <div className="account-content-wrapper">
-                        <h3 className="account-title">Argent Bank Savings (x6712)</h3>
-                        <p className="account-amount">$10,928.42</p>
-                        <p className="account-amount-description">Available Balance</p>
-                    </div>
-                    <div className="account-content-wrapper cta">
-                        <button className="transaction-button">View transactions</button>
-                    </div>
-                </section>
-                <section className="account">
-                    <div className="account-content-wrapper">
-                        <h3 className="account-title">Argent Bank Credit Card (x8349)</h3>
-                        <p className="account-amount">$184.30</p>
-                        <p className="account-amount-description">Current Balance</p>
-                    </div>
-                    <div className="account-content-wrapper cta">
-                        <button className="transaction-button">View transactions</button>
-                    </div>
-                </section>
+                <Account
+                    title="Argent Bank"
+                    accountCat="Checking"
+                    accountNumber="x8349"
+                    amount="$2,082.79"
+                    description="Available Balance"
+                />
+                <Account
+                    title="Argent Bank"
+                    accountCat="Savings"
+                    accountNumber="x6712"
+                    amount="$10,928.42"
+                    description="Available Balance"
+                />
+                <Account
+                    title="Argent Bank"
+                    accountCat="Credit Card"
+                    accountNumber="x8349"
+                    amount="$184.30"
+                    description="Current Balance"
+                />
             </main>
             <Footer />
         </div>
